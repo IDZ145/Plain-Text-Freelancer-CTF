1c81cbf37c0ee7c8d9e99efa79494e8df6f3baa6
Alex <git@alexlinker.com>
2016-04-09 14:22:30 +1000

    added obfuscated stuff ive been doijng

---
+++ obfuscated.c
@@ -0,0 +1,36 @@
+;_; // this code makes me sad // ;_;
+#include <stdio.h>
+#define UNLEASH_THE_POWER_OF(x) x%:%:_unlocked
+main(c, nope, not_at_all, not_one_bit, spam, spamming,
+not_designed_for_and, roid,
+a,b,c,d,e,f,g,h,i,j,,,elemeno,p,q,r,s,t,u,v,w,x,y,z,
+now,that,ive,just,then,given,my,abcs,to,you,next,time,pls,write,better,code,;,
+not,sure,how,many,more,variables,needed,here,
+argc, argv
+,__, // ,__, // ,__, // ,__,
+define)
+{
+	__??'=??-__;//??/
+	#define while(X) \
+	if ((X)+__) goto end;
+	??<define =;, 420 / 6; %>
+	#define UNLEASH_THE_POWER_OF(x) x%:%:_unlocked
+	*(char**)&argc = *(char**)&c = (define , main , &roid) ;
+	for (; *(int**)&c < *(int**)&argc+10 ;) *(*((int**)&c))++ = 0xBADC0DE ;
+	*(char**)&c = *(char**)&argc ;
+	start:
+	while(__??'UNLEASH_THE_POWER_OF(getchar)()) {
+	   _=__&-__ ,
+	   define = UNLEASH_THE_POWER_OF(getchar)()+_ ;
+	   (*(*((char**)&c))++ =define) ; ;
+	   goto start ;
+	}
+	%:define one <<1??!1)
+	end: // end
+	_+=-_-( define , ( ( ( ( ( ( 0 one one one *2) *2) one *2) ;
+	_*=__ ;
+	freopen(*(char**)&argc, (char*)&_, stdin);
+	for(;( **(char**)&argc = UNLEASH_THE_POWER_OF(getchar)())>__;) { // for loops are more robust
+		UNLEASH_THE_POWER_OF(putchar)(**(char**)&argc) ;
+	}
+}
